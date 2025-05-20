60b7b195b8a8477d2bf613ef8ef82e9b8c16af76
rammani_pop_os <rammani@abc.com>
2025-05-20 15:12:37 +0545

    feat: add 01basicsreact project

---
+++ 01basicsreact/src/App.js
@@ -0,0 +1,11 @@
+import Chaih3 from "./Chai";
+
+function App() {
+  return (
+    <>
+     <Chaih3 />
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicsreact/src/Chai.jsx
@@ -0,0 +1,4 @@
+function Chaih3 (){
+    return <h3>chai h3 in basics</h3>
+}
+export default Chaih3
