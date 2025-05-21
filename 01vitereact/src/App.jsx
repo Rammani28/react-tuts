993d3972e3bbde9644a9dc70a7cfb64f7ddd8af0
rammani_pop_os <rammani@abc.com>
2025-05-21 15:42:15 +0545

    learn:create react project with viet

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+import Chaih3 from "./chai.jsx";
+function App() {
+  return (
+    <>
+      <Chaih3 />
+      <h1>H1</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,5 @@
+function Chaih3() {
+  return <h3> chaih3 is ready</h3>;
+}
+
+export default Chaih3; 
